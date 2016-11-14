57b4ddd8b841ff670b3e7d148d2e7a10d92064a0
vm-agoralogie <virginie.moore@agoralogie.fr>
2016-11-14 14:59:21 +0100

    tableau init tension suivant tube type en cours

--- www/js/demo.js
+++ www/js/demo.js
@@ -717,7 +717,13 @@ app.controller('MainController', function(cordovaReady,$rootScope, $scope,$locat
 		var tension = 380;
 		
 		//$scope.connectedDevice.sensorType
-		alertNotif($scope.connectedDevice.sensorType+" tets","Failure","Ok")
+		alertNotif($scope.connectedDevice.tubeType+" tets","Failure","Ok");
+		
+		dataTemp = {};
+		dataTemp['4']=	"0x"+ tensions_tube[$scope.connectedDevice.tubeType]["tension_hexa"].charAt(0) + tensions_tube["SBM-20"]["tension_hexa"].charAt(1);
+		dataTemp['3']=	"0x"+ tensions_tube[$scope.connectedDevice.tubeType]["tension_hexa"].charAt(2) + tensions_tube["SBM-20"]["tension_hexa"].charAt(3);
+		dataTemp['2']=	"0x"+ tensions_tube[$scope.connectedDevice.tubeType]["tension_hexa"].charAt(4) + tensions_tube["SBM-20"]["tension_hexa"].charAt(5);
+		dataTemp['1']=	"0x"+ tensions_tube[$scope.connectedDevice.tubeType]["tension_hexa"].charAt(6) + tensions_tube["SBM-20"]["tension_hexa"].charAt(7);
 
 		data[4]="0x43";
 		data[3]="0xBE";
