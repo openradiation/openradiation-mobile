83eef46a80bc33f2e88e6385792818ef4d8be062
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2016-11-18 15:14:08 +0100

    ajout STS-5

--- www/js/init.js
+++ www/js/init.js
@@ -6,6 +6,7 @@ var tension_tube = {"SBM-20": 380, "M4011": 400};
 var tensions_tube =  {};
 tensions_tube["SBM-20"] 	= 	{"tension_hexa": '43be0000', "tension_min": 340, "tension_max": 400};
 tensions_tube["M4011"] 		=	{"tension_hexa": '43c80000', "tension_min": 340, "tension_max": 400};
+tensions_tube["STS-5"] 		=	{"tension_hexa": '43c80000', "tension_min": 340, "tension_max": 400};
 
 var OUT_PACKET_SERIAL_NB   		= 0x01;
 var OUT_PACKET_VERSION                  = 0x02;
