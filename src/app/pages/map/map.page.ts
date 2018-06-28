8db0c98bf5f5a4291731596da83fa0c0dafeea31
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-06-28 13:53:07 +0200

    Fix map page for serve testing

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -3,6 +3,7 @@ import { environment } from '../../../environments/environment';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Diagnostic } from '@ionic-native/diagnostic/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-map',
@@ -12,20 +13,29 @@ import { Diagnostic } from '@ionic-native/diagnostic/ngx';
 export class MapPage {
   iframeURL: SafeResourceUrl;
 
-  constructor(private domSanitizer: DomSanitizer, private geolocation: Geolocation, private diagnostic: Diagnostic) {
-    this.diagnostic.isLocationAvailable().then(locationAvailable => {
-      if (locationAvailable) {
-        this.geolocation.getCurrentPosition().then(geoposition => {
-          const zoom = 12;
-          const lat = geoposition.coords.latitude.toFixed(7);
-          const long = geoposition.coords.longitude.toFixed(7);
-          this.iframeURL = domSanitizer.bypassSecurityTrustResourceUrl(
-            `${environment.INAPPBROWSER_URI}/${zoom}/${lat}/${long}`
-          );
-        });
-      } else {
-        this.iframeURL = domSanitizer.bypassSecurityTrustResourceUrl(environment.INAPPBROWSER_URI);
-      }
-    });
+  constructor(
+    private domSanitizer: DomSanitizer,
+    private geolocation: Geolocation,
+    private diagnostic: Diagnostic,
+    private platform: Platform
+  ) {
+    if (this.platform.is('cordova')) {
+      this.diagnostic.isLocationAvailable().then(locationAvailable => {
+        if (locationAvailable) {
+          this.geolocation.getCurrentPosition().then(geoposition => {
+            const zoom = 12;
+            const lat = geoposition.coords.latitude.toFixed(7);
+            const long = geoposition.coords.longitude.toFixed(7);
+            this.iframeURL = domSanitizer.bypassSecurityTrustResourceUrl(
+              `${environment.INAPPBROWSER_URI}/${zoom}/${lat}/${long}`
+            );
+          });
+        } else {
+          this.iframeURL = domSanitizer.bypassSecurityTrustResourceUrl(environment.INAPPBROWSER_URI);
+        }
+      });
+    } else {
+      this.iframeURL = domSanitizer.bypassSecurityTrustResourceUrl(environment.INAPPBROWSER_URI);
+    }
   }
 }
