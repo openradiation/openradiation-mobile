c744ebece2817bb66bd39e9246d3f41162f47264
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-07-10 18:40:31 +0200

    Added about page

---
+++ src/app/components/accordion/accordion.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss']
+})
+export class AccordionComponent {
+  @Input() opened = true;
+
+  @Input() title: string;
+
+  @Output() open = new EventEmitter();
+}
