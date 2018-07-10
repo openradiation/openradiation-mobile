c59a7e24146b108dea1940c3bc58bab99e2f2788
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-07-10 18:46:14 +0200

    Added legal notice page

--- src/app/pages/legal-notice/legal-notice.page.ts
+++ src/app/pages/legal-notice/legal-notice.page.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-legal-notice',
   templateUrl: './legal-notice.page.html',
   styleUrls: ['./legal-notice.page.scss']
 })
-export class LegalNoticePage {}
+export class LegalNoticePage {
+  panelOpened = 1;
+  appliNameVersion = environment.appliNameVersion;
+}
