d7567a633a6f65d63d15121fcd453f8b2edab149
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-08-17 17:35:33 +0200

    Add mandatory login to start manual measures

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { AlertController, MenuController } from '@ionic/angular';
 import { NavigationEnd, Router } from '@angular/router';
-import { Actions, ofActionSuccessful, Store } from '@ngxs/store';
-import { filter } from 'rxjs/operators';
+import { Actions, ofActionSuccessful, Select, Store } from '@ngxs/store';
+import { filter, take } from 'rxjs/operators';
 import { StartManualMeasure } from '../../states/measures/measures.action';
+import { UserState } from '../../states/user/user.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -11,21 +13,24 @@ import { StartManualMeasure } from '../../states/measures/measures.action';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  @Select(UserState.login)
+  login$: Observable<string | undefined>;
+
   currentUrl: string;
 
   constructor(
     private menuController: MenuController,
     private router: Router,
     private store: Store,
-    private actions$: Actions
+    private actions$: Actions,
+    private alertController: AlertController
   ) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => (this.currentUrl = event.url));
-    this.actions$.pipe(ofActionSuccessful(StartManualMeasure)).subscribe(() => {
-      this.closeMenu();
-      this.router.navigate(['measure', 'manual']);
-    });
+    this.actions$
+      .pipe(ofActionSuccessful(StartManualMeasure))
+      .subscribe(() => this.router.navigate(['measure', 'manual']));
   }
 
   closeMenu() {
@@ -33,6 +38,48 @@ export class MenuComponent {
   }
 
   startManualMeasure() {
-    this.store.dispatch(new StartManualMeasure());
+    this.closeMenu();
+    this.login$.pipe(take(1)).subscribe(login => {
+      if (login !== undefined) {
+        this.store.dispatch(new StartManualMeasure());
+      } else {
+        this.goToLogin();
+      }
+    });
+  }
+
+  private goToLogin() {
+    this.alertController
+      .create({
+        header: 'Saisie manuelle',
+        message: `Vous devez vous identifier pour pouvoir saisir manuellemment une mesure`,
+        backdropDismiss: false,
+        buttons: [
+          {
+            text: 'Annuler'
+          },
+          {
+            text: `S'identifier`,
+            handler: () => {
+              this.router.navigate(
+                [
+                  'tabs',
+                  {
+                    outlets: {
+                      settings: 'log-in',
+                      home: null,
+                      history: null,
+                      map: null,
+                      other: null
+                    }
+                  }
+                ],
+                { queryParams: { startMeasureAfterLogin: true } }
+              );
+            }
+          }
+        ]
+      })
+      .then(alert => alert.present());
   }
 }
