62a97727d29a64a6aeba61c4acc6c84302734770
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-08-20 14:58:00 +0200

    Fix base test for Tabs

--- src/app/pages/tabs/tabs.component.spec.ts
+++ src/app/pages/tabs/tabs.component.spec.ts
@@ -1,21 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TabsPage } from './tabs.page';
+import { Tabs } from './tabs.component';
 
-describe('TabsPage', () => {
-  let component: TabsPage;
-  let fixture: ComponentFixture<TabsPage>;
+describe('Tabs', () => {
+  let component: Tabs;
+  let fixture: ComponentFixture<Tabs>;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [TabsPage],
+      declarations: [Tabs],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TabsPage);
+    fixture = TestBed.createComponent(Tabs);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
