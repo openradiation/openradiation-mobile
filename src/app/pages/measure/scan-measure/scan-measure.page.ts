71e0facbd048cc0ab04ca5c605e6fe1513a2dd7b
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-08-22 12:06:55 +0200

    Added base of scanMeasure page

--- src/app/pages/measure/scan-measure/scan-measure.page.ts
+++ src/app/pages/measure/scan-measure/scan-measure.page.ts
@@ -1,22 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Select, Store } from '@ngxs/store';
+import { MeasuresState } from '../../../states/measures/measures.state';
+import { Observable } from 'rxjs';
+import { Measure, PositionAccuracy } from '../../../states/measures/measure';
+import { AutoUnsubscribePage } from '../../auto-unsubscribe.page';
+import { StartWatchPosition, StopWatchPosition } from '../../../states/measures/measures.action';
 
 @Component({
   selector: 'app-scan-measure',
   templateUrl: './scan-measure.page.html',
   styleUrls: ['./scan-measure.page.scss']
 })
-export class ScanMeasurePage {
-  constructor(private router: Router) {}
+export class ScanMeasurePage extends AutoUnsubscribePage {
+  @Select(MeasuresState.positionAccuracy)
+  positionAccuracy$: Observable<PositionAccuracy>;
 
-  goToHome() {
-    this.router.navigate([
-      'tabs',
-      {
-        outlets: {
-          home: 'home'
-        }
-      }
-    ]);
+  @Select(MeasuresState.currentMeasure)
+  currentMeasure$: Observable<Measure>;
+
+  constructor(private store: Store, private router: Router) {
+    super();
   }
+
+  ionViewDidEnter() {
+    this.store.dispatch(new StartWatchPosition());
+  }
+
+  ionViewWillLeave() {
+    super.ionViewWillLeave();
+    this.store.dispatch(new StopWatchPosition());
+  }
+
+  endMeasure() {}
 }
