f97a1f492361b8da7bde29d890b3ccdc3b48176f
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-08-27 14:04:04 +0200

    Improved gpsIndicator

--- src/app/components/gps-indicator/gps-indicator.component.ts
+++ src/app/components/gps-indicator/gps-indicator.component.ts
@@ -1,14 +1,28 @@
-import { Component, Input } from '@angular/core';
-import { PositionAccuracy } from '../../states/measures/measure';
+import { Component, Input, OnChanges } from '@angular/core';
+import { PositionAccuracy, PositionAccuracyThreshold } from '../../states/measures/measure';
 
 @Component({
   selector: 'app-gps-indicator',
   templateUrl: './gps-indicator.component.html',
   styleUrls: ['./gps-indicator.component.scss']
 })
-export class GpsIndicatorComponent {
+export class GpsIndicatorComponent implements OnChanges {
   @Input()
-  positionAccuracy: number;
+  accuracy: number;
 
+  @Input()
+  compact = false;
+
+  positionAccuracy: PositionAccuracy;
   positionAccuracyEnum = PositionAccuracy;
+
+  ngOnChanges() {
+    if (!this.accuracy || this.accuracy <= PositionAccuracyThreshold.Error) {
+      this.positionAccuracy = PositionAccuracy.Error;
+    } else if (this.accuracy <= PositionAccuracyThreshold.Good) {
+      this.positionAccuracy = PositionAccuracy.Good;
+    } else {
+      this.positionAccuracy = PositionAccuracy.Bad;
+    }
+  }
 }
