3f0d0f4cebb1aedaa22da717fea82dca5bd82225
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-08-29 16:14:16 +0200

    Add confirmation meassge before measure delete or publish

--- src/app/pages/tabs/history/history.page.ts
+++ src/app/pages/tabs/history/history.page.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Measure } from '../../../states/measures/measure';
 import { Select, Store } from '@ngxs/store';
 import { DeleteMeasure, PublishMeasure } from '../../../states/measures/measures.action';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-history',
@@ -14,15 +15,45 @@ export class HistoryPage {
   @Select(MeasuresState.measures)
   measures$: Observable<Measure[]>;
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private alertController: AlertController) {}
 
   showDetail(measure: Measure) {}
 
   delete(measure: Measure) {
-    this.store.dispatch(new DeleteMeasure(measure));
+    this.alertController
+      .create({
+        header: 'Historique',
+        message: `Êtes-vous sûr(e) de vouloir supprimer cette mesure ?`,
+        backdropDismiss: false,
+        buttons: [
+          {
+            text: 'Non'
+          },
+          {
+            text: 'Oui',
+            handler: () => this.store.dispatch(new DeleteMeasure(measure))
+          }
+        ]
+      })
+      .then(alert => alert.present());
   }
 
   publish(measure: Measure) {
-    this.store.dispatch(new PublishMeasure(measure));
+    this.alertController
+      .create({
+        header: 'Historique',
+        message: `Êtes-vous sûr(e) de vouloir envoyer cette mesure ? En envoyant cette measure, vous acceptez que l'ensemble des données correspondant à cette measure (dont votre position GPS, votre peseudo, etc) soit publié dans la base OpenRadiation.`,
+        backdropDismiss: false,
+        buttons: [
+          {
+            text: 'Non'
+          },
+          {
+            text: 'Oui',
+            handler: () => this.store.dispatch(new PublishMeasure(measure))
+          }
+        ]
+      })
+      .then(alert => alert.present());
   }
 }
