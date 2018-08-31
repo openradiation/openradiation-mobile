cda8c9f0b4bd9b89a3f5bd2652c638dd1f52c1e6
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-08-31 15:33:30 +0200

    Added translation for measures-param page

--- src/app/pages/tabs/settings/measures-param/measures-param.module.ts
+++ src/app/pages/tabs/settings/measures-param/measures-param.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { ComponentsModule } from '../../../../components/components.module';
 import { MeasuresParamPage } from './measures-param.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [
   {
@@ -16,7 +17,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, RouterModule.forChild(routes), ComponentsModule],
+  imports: [CommonModule, FormsModule, IonicModule, RouterModule.forChild(routes), ComponentsModule, TranslateModule],
   declarations: [MeasuresParamPage]
 })
 export class MeasuresParamPageModule {}
