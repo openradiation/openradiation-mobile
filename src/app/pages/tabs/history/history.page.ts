e634c30934694a16b8284847405ef36428105a38
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-09-03 17:13:40 +0200

    Add animation when sending measure from history page

--- src/app/pages/tabs/history/history.page.ts
+++ src/app/pages/tabs/history/history.page.ts
@@ -1,26 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-import { Select, Store } from '@ngxs/store';
+import { Actions, ofActionDispatched, ofActionSuccessful, Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Measure } from '../../../states/measures/measure';
 import { DeleteAllMeasures, DeleteMeasure, PublishMeasure } from '../../../states/measures/measures.action';
 import { MeasuresState } from '../../../states/measures/measures.state';
 import { TranslateService } from '@ngx-translate/core';
+import { AutoUnsubscribePage } from '../../../components/auto-unsubscribe/auto-unsubscribe.page';
+import { TabsService } from '../tabs.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.page.html',
   styleUrls: ['./history.page.scss']
 })
-export class HistoryPage {
+export class HistoryPage extends AutoUnsubscribePage {
   @Select(MeasuresState.measures)
   measures$: Observable<Measure[]>;
 
+  measureBeingSentMap: { [K: string]: boolean } = {};
+
   constructor(
+    protected tabsService: TabsService,
+    protected elementRef: ElementRef,
     private store: Store,
     private alertController: AlertController,
-    private translateService: TranslateService
-  ) {}
+    private translateService: TranslateService,
+    private actions$: Actions
+  ) {
+    super(tabsService, elementRef);
+  }
+
+  ionViewDidEnter() {
+    super.ionViewDidEnter();
+    this.subscriptions.push(
+      this.actions$.pipe(ofActionDispatched(PublishMeasure)).subscribe((action: PublishMeasure) => {
+        this.measureBeingSentMap[action.measure.reportUuid] = true;
+      }),
+      this.actions$
+        .pipe(ofActionSuccessful(PublishMeasure))
+        .subscribe((action: PublishMeasure) => (this.measureBeingSentMap[action.measure.reportUuid] = false))
+    );
+  }
 
   // TODO implement
   showDetail(measure: Measure) {}
