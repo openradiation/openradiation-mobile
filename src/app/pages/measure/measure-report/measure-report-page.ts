4396b6128dde005662438e97e6d60fe0d62f9227
vcroayne <vcroayne@itlink.fr>
2018-10-02 18:21:17 +0200

    Add photo component in report page

--- src/app/pages/measure/measure-report/measure-report-page.ts
+++ src/app/pages/measure/measure-report/measure-report-page.ts
@@ -131,6 +131,7 @@ export class MeasureReportPage extends AutoUnsubscribePage {
           this.measureReportForm.get('rain')!.disable();
           this.measureReportForm.get('description')!.disable();
           this.measureReportForm.get('tags')!.disable();
+          this.measureReportForm.get('photo')!.disable();
         }
       }
       this.init();
