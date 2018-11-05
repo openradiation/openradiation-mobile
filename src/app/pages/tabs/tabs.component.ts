3e7c00da1fc4af4214e230253bf109abe203a0c7
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-11-05 11:52:43 +0100

    Updated dependencies for ionic

--- src/app/pages/tabs/tabs.component.ts
+++ src/app/pages/tabs/tabs.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { NavigationStart, Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tabs',
@@ -9,9 +7,9 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['tabs.component.scss']
 })
 export class Tabs {
-  constructor(private menuController: MenuController, private router: Router) {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationStart && event.url === '/#'))
-      .subscribe(() => this.menuController.open());
+  constructor(private menuController: MenuController) {}
+
+  openMenu() {
+    this.menuController.open();
   }
 }
