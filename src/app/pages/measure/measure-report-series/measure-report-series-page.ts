f733c6907b7f5c41521ab40c91a744c3be0d1e95
vcroayne <vcroayne@itlink.fr>
2018-11-07 14:17:29 +0100

    First version report page dedicated to measurement series report

--- src/app/pages/measure/measure-report-series/measure-report-series-page.ts
+++ src/app/pages/measure/measure-report-series/measure-report-series-page.ts
@@ -6,7 +6,7 @@ import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { AutoUnsubscribePage } from '../../../components/auto-unsubscribe/auto-unsubscribe.page';
 import { SelectIconOption } from '../../../components/select-icon/select-icon-option';
-import { Measure, PositionAccuracyThreshold } from '../../../states/measures/measure';
+import { Measure, MeasureEnvironment, PositionAccuracyThreshold } from '../../../states/measures/measure';
 import { MeasuresState } from '../../../states/measures/measures.state';
 import { UserState } from '../../../states/user/user.state';
 
@@ -31,6 +31,69 @@ export class MeasureReportSeriesPage extends AutoUnsubscribePage {
 
   url = '/measure/report-series';
 
+  measurementEnvironmentOptions: SelectIconOption[] = [
+    {
+      iconOn: 'assets/img/icon-countryside-on.png',
+      iconOff: 'assets/img/icon-countryside-off.png',
+      label: <string>_('MEASURES.ENVIRONMENT.COUNTRYSIDE'),
+      value: MeasureEnvironment.Countryside
+    },
+    {
+      iconOn: 'assets/img/icon-city-on.png',
+      iconOff: 'assets/img/icon-city-off.png',
+      label: <string>_('MEASURES.ENVIRONMENT.CITY'),
+      value: MeasureEnvironment.City
+    },
+    {
+      iconOn: 'assets/img/icon-inside-on.png',
+      iconOff: 'assets/img/icon-inside-off.png',
+      label: <string>_('MEASURES.ENVIRONMENT.INSIDE'),
+      value: MeasureEnvironment.Inside
+    },
+    {
+      iconOn: 'assets/img/icon-ontheroad-on.png',
+      iconOff: 'assets/img/icon-ontheroad-off.png',
+      label: <string>_('MEASURES.ENVIRONMENT.ON_THE_ROAD'),
+      value: MeasureEnvironment.OnTheRoad
+    },
+    {
+      iconOn: 'assets/img/icon-plane-on.png',
+      iconOff: 'assets/img/icon-plane-off.png',
+      label: <string>_('MEASURES.ENVIRONMENT.PLANE'),
+      value: MeasureEnvironment.Plane
+    }
+  ];
+
+  measurementHeightOptions: SelectIconOption[] = [
+    {
+      iconOn: 'assets/img/icon-floor-on.png',
+      iconOff: 'assets/img/icon-floor-off.png',
+      label: <string>_('MEASURES.SENSOR_POSITION.FLOOR'),
+      value: 0
+    },
+    {
+      iconOn: 'assets/img/icon-elevated-on.png',
+      iconOff: 'assets/img/icon-elevated-off.png',
+      label: <string>_('MEASURES.SENSOR_POSITION.1_METER_HIGH'),
+      value: 1
+    }
+  ];
+
+  rainOptions: SelectIconOption[] = [
+    {
+      iconOn: 'assets/img/icon-sun-on.png',
+      iconOff: 'assets/img/icon-sun-off.png',
+      label: <string>_('MEASURES.WEATHER.NO_RAIN'),
+      value: false
+    },
+    {
+      iconOn: 'assets/img/icon-rain-on.png',
+      iconOff: 'assets/img/icon-rain-off.png',
+      label: <string>_('MEASURES.WEATHER.RAIN'),
+      value: true
+    }
+  ];
+
   constructor(protected router: Router) {
     super(router);
   }
@@ -38,4 +101,8 @@ export class MeasureReportSeriesPage extends AutoUnsubscribePage {
   pageEnter() {
     super.pageEnter();
   }
+
+  stopReportSeries() {}
+
+  cancelMeasure() {}
 }
