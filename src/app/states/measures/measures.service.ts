7531ecb03709dbcf64c220dbdee593a3a74d4117
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-11-13 10:05:18 +0100

    Added base to send measureSeries

--- src/app/states/measures/measures.service.ts
+++ src/app/states/measures/measures.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Actions, ofActionSuccessful, Store } from '@ngxs/store';
-import { interval, Observable, of } from 'rxjs';
+import { forkJoin, interval, Observable, of } from 'rxjs';
 import { filter, shareReplay, take, takeUntil, tap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 import { AbstractDevice } from '../devices/abstract-device';
@@ -69,60 +69,64 @@ export class MeasuresService {
   publishMeasure(measure: Measure | MeasureSeries): Observable<any> {
     switch (measure.type) {
       case MeasureType.Measure: {
-        if (
-          measure.accuracy &&
-          measure.accuracy < PositionAccuracyThreshold.No &&
-          measure.endAccuracy &&
-          measure.endAccuracy < PositionAccuracyThreshold.No
-        ) {
-          const payload: MeasureApi = {
-            apiKey: environment.API_KEY,
-            data: {
-              apparatusId: measure.apparatusId,
-              apparatusVersion: measure.apparatusVersion,
-              apparatusSensorType: measure.apparatusSensorType,
-              apparatusTubeType: measure.apparatusTubeType,
-              temperature: measure.temperature ? Math.round(measure.temperature) : undefined,
-              value: measure.value,
-              hitsNumber: measure.hitsNumber,
-              startTime: new Date(measure.startTime).toISOString(),
-              endTime: measure.endTime ? new Date(measure.endTime).toISOString() : undefined,
-              latitude: measure.latitude!,
-              longitude: measure.longitude!,
-              accuracy: measure.accuracy,
-              altitude: measure.altitude ? Math.round(measure.altitude) : undefined,
-              altitudeAccuracy: measure.altitudeAccuracy,
-              endLatitude: measure.endLatitude,
-              endLongitude: measure.endLongitude,
-              endAccuracy: measure.endAccuracy,
-              endAltitude: measure.endAltitude ? Math.round(measure.endAltitude) : undefined,
-              endAltitudeAccuracy: measure.endAltitudeAccuracy,
-              deviceUuid: measure.deviceUuid,
-              devicePlatform: measure.devicePlatform,
-              deviceVersion: measure.deviceVersion,
-              deviceModel: measure.deviceModel,
-              reportUuid: measure.reportUuid,
-              manualReporting: measure.manualReporting,
-              organisationReporting: measure.organisationReporting,
-              reportContext: 'routine',
-              description: measure.description,
-              measurementHeight: measure.measurementHeight,
-              tags: measure.tags,
-              enclosedObject: measure.enclosedObject,
-              userId: this.store.selectSnapshot(({ user }: { user: UserStateModel }) => user.login),
-              userPwd: this.store.selectSnapshot(({ user }: { user: UserStateModel }) => user.password),
-              measurementEnvironment: measure.measurementEnvironment,
-              rain: measure.rain
-            }
-          };
-          return this.httpClient.post(environment.API_URI, payload);
-        } else {
-          throw new Error('missing start or end position in measure');
-        }
+        return this.postMeasure(measure);
       }
       case MeasureType.MeasureSeries: {
-        return of(null);
+        return forkJoin(measure.measures.map(subMeasure => this.postMeasure(subMeasure)));
       }
+    }
+  }
+
+  private postMeasure(measure: Measure): Observable<any> {
+    if (
+      measure.accuracy &&
+      measure.accuracy < PositionAccuracyThreshold.No &&
+      measure.endAccuracy &&
+      measure.endAccuracy < PositionAccuracyThreshold.No
+    ) {
+      const payload: MeasureApi = {
+        apiKey: environment.API_KEY,
+        data: {
+          apparatusId: measure.apparatusId,
+          apparatusVersion: measure.apparatusVersion,
+          apparatusSensorType: measure.apparatusSensorType,
+          apparatusTubeType: measure.apparatusTubeType,
+          temperature: measure.temperature ? Math.round(measure.temperature) : undefined,
+          value: measure.value,
+          hitsNumber: measure.hitsNumber,
+          startTime: new Date(measure.startTime).toISOString(),
+          endTime: measure.endTime ? new Date(measure.endTime).toISOString() : undefined,
+          latitude: measure.latitude!,
+          longitude: measure.longitude!,
+          accuracy: measure.accuracy,
+          altitude: measure.altitude ? Math.round(measure.altitude) : undefined,
+          altitudeAccuracy: measure.altitudeAccuracy,
+          endLatitude: measure.endLatitude,
+          endLongitude: measure.endLongitude,
+          endAccuracy: measure.endAccuracy,
+          endAltitude: measure.endAltitude ? Math.round(measure.endAltitude) : undefined,
+          endAltitudeAccuracy: measure.endAltitudeAccuracy,
+          deviceUuid: measure.deviceUuid,
+          devicePlatform: measure.devicePlatform,
+          deviceVersion: measure.deviceVersion,
+          deviceModel: measure.deviceModel,
+          reportUuid: measure.reportUuid,
+          manualReporting: measure.manualReporting,
+          organisationReporting: measure.organisationReporting,
+          reportContext: 'routine',
+          description: measure.description,
+          measurementHeight: measure.measurementHeight,
+          tags: measure.tags,
+          enclosedObject: measure.enclosedObject,
+          userId: this.store.selectSnapshot(({ user }: { user: UserStateModel }) => user.login),
+          userPwd: this.store.selectSnapshot(({ user }: { user: UserStateModel }) => user.password),
+          measurementEnvironment: measure.measurementEnvironment,
+          rain: measure.rain
+        }
+      };
+      return this.httpClient.post(environment.API_URI, payload);
+    } else {
+      return of(null);
     }
   }
 }
