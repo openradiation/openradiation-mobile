1b1044061ffb81b4f6719a2a6a22dfc9c4c47737
vcroayne <vcroayne@itlink.fr>
2018-12-11 13:49:49 +0100

    Not finished, connection ok, try to start a measure

--- src/app/states/devices/usb/device-rium.service.ts
+++ src/app/states/devices/usb/device-rium.service.ts
@@ -5,7 +5,6 @@ import { Observable, of } from 'rxjs';
 import { filter, map, startWith, takeUntil } from 'rxjs/operators';
 import { Step } from '../../measures/measure';
 import { AbstractUSBDeviceService } from './abstract-usb-device.service';
-import { DevicePocketGeiger } from './device-pocket-geiger';
 import { DeviceRium } from './device-rium';
 
 @Injectable({
@@ -17,6 +16,7 @@ export class DeviceRiumService extends AbstractUSBDeviceService<DeviceRium> {
   }
 
   protected convertHitsNumberPerSec(hitsNumberPerSec: number): number {
+    // TODO fix conversion factor
     return (hitsNumberPerSec * 60) / 53.032;
   }
 
@@ -29,16 +29,38 @@ export class DeviceRiumService extends AbstractUSBDeviceService<DeviceRium> {
   }
 
   startMeasureScan(device: DeviceRium, stopSignal: Observable<any>): Observable<Step> {
-    return this.serial.registerReadCallback().pipe(
-      takeUntil(stopSignal),
-      filter((buffer: any): buffer is ArrayBuffer => buffer instanceof ArrayBuffer),
-      map((buffer: ArrayBuffer) => this.decodeDataPackage(buffer)),
-      startWith({ ts: Date.now(), hitsNumber: 0 }),
-      filter((step: Step | null): step is Step => step !== null)
-    );
+    console.log('startmeasure');
+    // let stop = false;
+    // while (!stop) {
+    //   console.log('in while');
+    this.serial
+      .read()
+      .then(console.log)
+      .catch(console.error)
+      .finally(() => this.serial.close());
+    //   stopSignal.subscribe(() => (stop = true));
+    // }
+    return of({ ts: Date.now(), hitsNumber: 0, temperature: 0 });
+    // return this.serial.registerReadCallback().pipe(
+    //   takeUntil(stopSignal),
+    //   filter((buffer: any): buffer is ArrayBuffer => buffer instanceof ArrayBuffer),
+    //   map((buffer: ArrayBuffer) => this.decodeDataPackage(buffer)),
+    //   startWith({ ts: Date.now(), hitsNumber: 0, temperature: 0 }),
+    //   filter((step: Step | null): step is Step => step !== null)
+    // );
   }
 
   protected decodeDataPackage(buffer: ArrayBuffer): Step | null {
+    const data = this.textDecoder.decode(buffer);
+    console.log('data', data);
+    const dataPackage = data.slice(1).split('-');
+    const hitsNumber = 1;
+    if (hitsNumber) {
+      return {
+        ts: Date.now(),
+        hitsNumber
+      };
+    }
     return null;
   }
 }
