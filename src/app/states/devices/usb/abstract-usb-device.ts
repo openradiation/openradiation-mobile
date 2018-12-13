54233ee9f61917909662062baa5bf13d31c0696d
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-12-13 14:51:44 +0100

    Fix the impossibility to override usb device driver

--- src/app/states/devices/usb/abstract-usb-device.ts
+++ src/app/states/devices/usb/abstract-usb-device.ts
@@ -3,7 +3,7 @@ import { AbstractDevice } from '../abstract-device';
 export abstract class AbstractUSBDevice extends AbstractDevice {
   abstract readonly pid: string;
   abstract readonly vid: string;
-  readonly driver = 'CdcAcmSerialDriver';
+  readonly driver: string = 'CdcAcmSerialDriver';
   abstract readonly baudRate: number;
   abstract readonly dataBits: number;
 }
