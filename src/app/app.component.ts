a5821db3838ee0ab5ed05d6874908a4d41229cc0
Tom Demulier--Chevret <tdemulier@itlink.fr>
2020-09-10 18:33:31 +0200

    Add base setup to receive push notifications on Android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MenuController, Platform } from '@ionic/angular';
+import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic/ngx';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,26 @@ import { MenuController, Platform } from '@ionic/angular';
 export class AppComponent {
   keyboardOpen: boolean;
 
-  constructor(private menuController: MenuController, private platform: Platform) {
+  constructor(private menuController: MenuController, private platform: Platform, private fcm: FCM) {
     this.initializeApp();
   }
 
   initializeApp() {
     window.addEventListener('keyboardWillShow', () => (this.keyboardOpen = true));
     window.addEventListener('keyboardWillHide', () => (this.keyboardOpen = false));
+    this.fcm.onNotification().subscribe(notification => console.log(notification));
+    this.fcm
+      .createNotificationChannel({
+        id: 'openradiation', // required
+        name: 'OpenRadiation', // required
+        description: 'OpenRadiation alert',
+        importance: 'high', // https://developer.android.com/guide/topics/ui/notifiers/notifications#importance
+        visibility: 'public', // https://developer.android.com/training/notify-user/build-notification#lockscreenNotification
+        sound: 'alert_sound', // In the "alert_sound" example, the file should located as resources/raw/alert_sound.mp3
+        lights: true, // enable lights for notifications
+        vibration: true // enable vibration for notifications
+      })
+      .then(() => this.fcm.subscribeToTopic('fr'));
   }
 
   onMenuOpen() {
