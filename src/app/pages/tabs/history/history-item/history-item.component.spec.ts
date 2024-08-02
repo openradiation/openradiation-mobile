fdd9ee7933fef5765c3c9967eb404982da9525c4
Alex Morel <amorel@codelutin.com>
2024-08-02 15:17:55 +0200

    [Tests] Fix History item tests

--- src/app/pages/tabs/history/history-item/history-item.component.spec.ts
+++ src/app/pages/tabs/history/history-item/history-item.component.spec.ts
@@ -3,6 +3,9 @@ import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HistoryItemComponent } from './history-item.component';
 
 import { getTestImports, getTestProviders } from '../../../../../tests/TestUtils'
+import { Measure } from '../../../../../app/states/measures/measure';
+import { ApparatusSensorType } from '../../../../../app/states/devices/abstract-device';
+import { DosePipe } from '../../../../..//app/components/pipes/dose/dose.pipe';
 
 describe('HistoryItemComponent', () => {
   let component: HistoryItemComponent;
@@ -10,16 +13,29 @@ describe('HistoryItemComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [HistoryItemComponent]
-      ,
-      imports: getTestImports(),
-      providers: getTestProviders()
+      declarations: [HistoryItemComponent, DosePipe],
+      imports:
+        getTestImports(),
+      providers:
+        getTestProviders(),
     }).compileComponents();
   }));
 
   beforeEach(() => {
+    const fakeMeasure = new Measure(
+      "apparatusId",
+      "apparatusVersion",
+      ApparatusSensorType.Geiger,
+      "apparatusTubeType",
+      "deviceUUID",
+      "devicePlatform",
+      "deviceOsVersion",
+      "deviceModel"
+    );
+    fakeMeasure.value = 42;
     fixture = TestBed.createComponent(HistoryItemComponent);
     component = fixture.componentInstance;
+    component.measure = fakeMeasure;
     fixture.detectChanges();
   });
 
