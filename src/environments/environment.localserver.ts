f37099639c60b0f1fec72bbcfae41e7e4d92ae97
Alex Morel <amorel@codelutin.com>
2025-03-11 14:30:48 +0100

    #11 Feedback page

--- src/environments/environment.localserver.ts
+++ src/environments/environment.localserver.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  production: true,
-  APP_NAME_VERSION: 'OpenRadiation app 3.1 beta',
+  production: false,
+  APP_NAME_VERSION: 'OpenRadiation app 3.1 mockDevice',
   API_KEY: '50adef3bdec466edc25f40c8fedccbce',
-  API_URI: 'https://submit.openradiation.preprod.ul2i.fr/measurements',
-  FEEDBACK_URI: 'https://submit.openradiation.preprod.ul2i.fr/feedback',
+  API_URI: 'http://localhost:8083/measurements',
+  FEEDBACK_URI: 'http://localhost:8083/feedback',
   FEEDBACK_EMAIL: 'irsn-openradiation@projet.codelutin.com',
   IN_APP_BROWSER_URI: {
     base: 'https://request.openradiation.preprod.ul2i.fr',
     suffix: 'openradiation',
   },
-  mockDevice: false,
+  mockDevice: true,
   isTestEnvironment: false,
 };
